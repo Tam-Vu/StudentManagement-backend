51eb475b83f752accf61d525f651b2b38c3b9b15
Tam-Vu <vuthanhtam12062003@gmail.com>
2024-05-18 23:52:11 +0700

    login logout

--- src/server.js
+++ src/server.js
@@ -13,7 +13,10 @@ import subjectResultRoute from "./routes/subjectResultRoute";
 import subjectRoute from "./routes/subjectRoute";
 import tuitionsRoute from "./routes/tuitionsRoute"
 import loginRoute from "./routes/loginRoute"
+import cookieParser from "cookie-parser"; 
+import {checkUserJwt} from "./middleware/jwtService"
 import cors from "cors";
+// import jwtAction from '../middleware/jwtService';
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
@@ -21,7 +24,16 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+// const checkUserLogin = (req, res, next) => {
+//   const nonSecurePath = ['/login'];
+//   if (nonSecurePath.includes(req.path)) return next();
+//   if(user) {
+//     next();
+//   }
+// }
+app.all('*', checkUserJwt);
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -43,6 +55,9 @@ subjectRoute(app);
 tuitionsRoute(app);
 loginRoute(app);
 Connection();
+app.use((req, res) => {
+  return res.send("404 not found");
+})
 app.listen(PORT, () => {
   console.log("backend is running in port: " + PORT);
 });
