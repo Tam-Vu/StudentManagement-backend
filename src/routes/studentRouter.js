8d02ce70f57cc2102f5661ee289c928cf1fe6dc9
Tam-Vu <vuthanhtam12062003@gmail.com>
2024-06-12 20:27:41 +0700

    aaaaa

--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -22,7 +22,6 @@ const studentRouter = (app) => {
   router.put("/update-student/:id/:btcId",studentController.handleUpdateStudent);
   router.put("/delete-student/:id", studentController.handleDeleteStudent);
   router.post("/create-student",upload.single("image"),studentController.handleCreateNewStudent);
-  // router.get("/student-without-class/:year", studentController.handleFindNonClassStudent);
   router.get("/student-without-class/:id",studentController.handleFindAllNonClassStudentByClassId);
   router.get("/student-in-year/:year", studentController.handleFindAllStudentInYear);
   return app.use("/student", router);
